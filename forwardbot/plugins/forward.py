91ba5ba991329c525c583e532d1b651869cdd992
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-07-14 11:02:16 +0530

    Update forward.py

--- forwardbot/plugins/forward.py
+++ forwardbot/plugins/forward.py
@@ -55,7 +55,7 @@ async def handler(event):
         await event.respond('**Select What you need to forward**', buttons=[
                     [Button.inline('ᴀʟʟ ᴍᴇssᴀɢᴇs', b'all'), Button.inline('ᴏɴʟʏ ᴘʜᴏᴛᴏs', b'photo')],
                     [Button.inline('ᴏɴʟʏ ᴅᴏᴄᴜᴍᴇɴᴛs', b'docs'), Button.inline('ᴏɴʟʏ ᴠɪᴅᴇᴏ' , b'video')],
-                    [Button.inline('ᴅᴏᴄᴜᴍᴇɴᴛꜱ ᴀɴᴅ ᴠɪᴅᴇᴏꜱ', b'docs' & b'video)]
+                    [Button.inline('ᴅᴏᴄᴜᴍᴇɴᴛꜱ ᴀɴᴅ ᴠɪᴅᴇᴏꜱ', b'docs' & b'video')]
                     ])
 
 @forwardbot_cmd("reset", is_args=False)
